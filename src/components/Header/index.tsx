461c06a5edffac06a505fc8a1f1dfdf9ee926acf
bruno <brunofay1@hotmail.com>
2022-09-27 17:44:38 -0300

    NEW add radix ui and implement dialog modal without css

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,13 +1,26 @@
 import React from 'react'
 import { HeaderContainer, HeaderContent, NewTransactionButton } from './styles'
 import Logo from '../../assets/Logo.svg'
+import * as Dialog from '@radix-ui/react-dialog'
 
 export default function Header() {
   return (
     <HeaderContainer>
       <HeaderContent>
         <img src={Logo} alt="" />
-        <NewTransactionButton>New transaction</NewTransactionButton>
+        <Dialog.Root>
+          <Dialog.Trigger asChild>
+          <NewTransactionButton>New transaction</NewTransactionButton>
+          </Dialog.Trigger>
+          <Dialog.Portal>
+            <Dialog.Overlay />
+            <Dialog.Content>
+              <Dialog.Title />
+              <Dialog.Description />
+              <Dialog.Close />
+            </Dialog.Content>
+          </Dialog.Portal>
+        </Dialog.Root>
       </HeaderContent>
     </HeaderContainer>
   )
