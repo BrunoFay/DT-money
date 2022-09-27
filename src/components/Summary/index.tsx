f73d048defe382b94c1587126a8b26a6ec89a546
bruno <brunofay1@hotmail.com>
2022-09-27 16:25:24 -0300

    NEW add summary component with css

---
+++ src/components/Summary/index.tsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import { SummaryCard, SummaryContainer } from './styles'
+import {ArrowCircleUp,ArrowCircleDown,CurrencyDollar} from 'phosphor-react'
+
+export default function Summary() {
+  return (
+    <SummaryContainer>
+      <SummaryCard>
+        <header>
+          <span>Entries</span>
+          <ArrowCircleUp size={32} color={'#00B37E'}/>
+        </header>
+        <strong>R$ 16.141,00</strong>
+      </SummaryCard>
+      <SummaryCard>
+        <header>
+          <span>Outlay</span>
+          <ArrowCircleDown size={32} color={'#F75A68'}/>
+        </header>
+        <strong>R$ 16.141,00</strong>
+      </SummaryCard>
+      <SummaryCard variant='green'>
+        <header>
+          <span>Total</span>
+          <CurrencyDollar size={32} color={'white'}/>
+        </header>
+        <strong>R$ 16.141,00</strong>
+      </SummaryCard>
+    </SummaryContainer>
+  )
+}
