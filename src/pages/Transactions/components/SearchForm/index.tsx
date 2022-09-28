fda8fce8b9fe8aac6c8820aa0819ebefd56e60d6
bruno <brunofay1@hotmail.com>
2022-09-28 17:40:42 -0300

    NEW add react hook form and zod, and add hook in search input

--- src/pages/Transactions/components/SearchForm/index.tsx
+++ src/pages/Transactions/components/SearchForm/index.tsx
@@ -1,13 +1,31 @@
 import React from 'react'
 import { SearchFormContainer } from './styles'
-import {MagnifyingGlass} from 'phosphor-react'
+import { MagnifyingGlass } from 'phosphor-react'
+import { useForm } from 'react-hook-form'
+import * as zod from 'zod'
+import { zodResolver } from '@hookform/resolvers'
+
+
+const searchZodSchema = zod.object({
+  search: zod.string()
+})
+
+type SearchInputForm = zod.infer<typeof searchZodSchema>
 
 export default function SearchForm() {
+  const { register, handleSubmit, formState: { isSubmitting } } = useForm<SearchInputForm>({
+    resolver: zodResolver(searchZodSchema)
+  })
+
+  function handleSearchForm(data: SearchInputForm) {
+    console.log(data);
+
+  }
   return (
-    <SearchFormContainer>
-      <input type="text" placeholder='Search by transactions' />
-      <button type='submit'>
-        <MagnifyingGlass size={20}/>
+    <SearchFormContainer onSubmit={handleSubmit(handleSearchForm)}>
+      <input type="text" placeholder='Search by transactions' {...register('search')} />
+      <button type='submit' disabled={isSubmitting}>
+        <MagnifyingGlass size={20} />
         Search
       </button>
     </SearchFormContainer>
