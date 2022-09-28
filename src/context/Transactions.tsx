60c1cfc481cbee488f479bf03a88e2eec710aff7
bruno <brunofay1@hotmail.com>
2022-09-28 18:38:52 -0300

    NEW add axios

--- src/context/Transactions.tsx
+++ src/context/Transactions.tsx
@@ -1,25 +1,31 @@
 import { createContext, ReactNode, useEffect, useState } from "react"
 import { Transaction } from "../@types/transactionPage"
+import { api } from "../api/axios"
 
 interface TransactionContext {
   transactions: Transaction[]
+  getAllTransactions: (s: string) => Promise<void>
 }
 export const transactionContext = createContext({} as TransactionContext)
 
 export function TransactionProvider({ children }: { children: ReactNode }) {
   const [transactions, setTransactions] = useState<Transaction[]>([])
 
-  async function getAllTransactions() {
-    const response = await fetch('http://localhost:3001/transactions')
-    const data: Transaction[] = await response.json()
-    setTransactions(data)
+  async function getAllTransactions(query?: string) {
+    let response = await api.get('transactions')
+    if (query) {
+      response = await api.get('transactions', { params: { q: query } })
+    }
+    console.log(response);
+    
+    setTransactions(response.data)
   }
 
   useEffect(() => {
     getAllTransactions()
   }, [])
 
-  const valueToProvide = { transactions }
+  const valueToProvide = { transactions, getAllTransactions }
   return (
     <transactionContext.Provider value={valueToProvide}>
       {children}
--- src/pages/Transactions/components/SearchForm/index.tsx
+++ src/pages/Transactions/components/SearchForm/index.tsx
@@ -1,9 +1,10 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { SearchFormContainer } from './styles'
 import { MagnifyingGlass } from 'phosphor-react'
 import { useForm } from 'react-hook-form'
 import * as zod from 'zod'
 import { zodResolver } from '@hookform/resolvers/zod'
+import { transactionContext } from '../../../../context/Transactions'
 
 
 const searchZodSchema = zod.object({
@@ -13,13 +14,14 @@ const searchZodSchema = zod.object({
 type SearchInputForm = zod.infer<typeof searchZodSchema>
 
 export default function SearchForm() {
+  const { getAllTransactions } = useContext(transactionContext)
   const { register, handleSubmit, formState: { isSubmitting } } = useForm<SearchInputForm>({
     resolver: zodResolver(searchZodSchema)
   })
 
-  function handleSearchForm(data: SearchInputForm) {
+  async function handleSearchForm(data: SearchInputForm) {
     console.log(data);
-
+    await getAllTransactions(data.search)
   }
   return (
     <SearchFormContainer onSubmit={handleSubmit(handleSearchForm)}>
