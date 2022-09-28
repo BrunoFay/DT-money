9f66282e8e5436bd5ec4ca4bc334e6ca10da93aa
bruno <brunofay1@hotmail.com>
2022-09-28 16:20:14 -0300

    NEW/REFACT add logic to get transactions from server and change static informations

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,31 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Transaction } from '../../@types/transactionPage'
 import Header from '../../components/Header'
 import Summary from '../../components/Summary'
 import SearchForm from './components/SearchForm'
 import { PriceTable, TransactionsContainer, TransactionTable } from './styles'
 
 export default function Transactions() {
+  const [transactions, setTransactions] = useState<Transaction[]>([])
+  async function getAllTransactions() {
+    const response = await fetch('http://localhost:3001/transactions')
+    const data: Transaction[] = await response.json()
+    setTransactions(data)
+  }
   return (
     <div>
-      <Header/>
-      <Summary/>
+      <Header />
+      <Summary />
       <TransactionsContainer>
-        <SearchForm/>
+        <SearchForm />
         <TransactionTable>
-         <tbody>
-          <tr>
-            <td width='43%'>Desenvolvimento de site</td>
-           <td> <PriceTable variant='income'>R$ 12.000,00</PriceTable></td>
-            <td>Venda</td>
-            <td>13/04/2022</td>
-          </tr>
-          <tr>
-            <td width='43%'>Hamburguer</td>
-           <td> <PriceTable variant='outcome'>- R$ 59,00</PriceTable></td>
-            <td>Alimentação</td>
-            <td>10/04/2022</td>
-          </tr>
-         </tbody>
+          <tbody>
+            {transactions.map(transaction => (
+              <tr key={transaction.id}>
+                <td width='43%'>{transaction.description}</td>
+                <td> <PriceTable variant={transaction.type}>R$ {transaction.price}</PriceTable></td>
+                <td>{transaction.category}</td>
+                <td>{transaction.createdAt}</td>
+              </tr>
+
+            ))}
+          </tbody>
         </TransactionTable>
       </TransactionsContainer>
     </div>
